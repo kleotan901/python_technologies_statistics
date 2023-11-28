a9b0c50105f6ffe1287f0849f4583382a9287b4a
Tetiana Kozakova <kleotan901@gmail.com>
2023-11-28 13:41:12 +0100

    Updated data_analysis.py file

--- data_analysis/data_analysis.py
+++ data_analysis/data_analysis.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import nltk
 import pandas as pd
 import seaborn as sns
+from nltk.corpus import stopwords
 from pandas import DataFrame
 from wordcloud import WordCloud
 
@@ -9,10 +10,14 @@ import config
 
 
 def get_count_technologies() -> DataFrame:
-    jobs_text = pd.read_csv("jobs.csv")
+    jobs_text = pd.read_csv("../scraping/jobs.csv")
     text = " ".join(jobs_text["description"].astype(str))
-    tokens = nltk.word_tokenize(text)
-    pos_tags = nltk.pos_tag(tokens)
+
+    # use nltk to get names of technologies
+    stop_words = set(stopwords.words("english"))
+    word_tokens = nltk.word_tokenize(text)
+    filtered_sentence = [word for word in word_tokens if not word.lower() in stop_words]
+    pos_tags = nltk.pos_tag(filtered_sentence)
 
     technologies_dict = {}
     for techs_tuple in pos_tags:
@@ -21,6 +26,15 @@ def get_count_technologies() -> DataFrame:
                 technologies_dict[techs_tuple[0]] += 1
             if techs_tuple[1] == "NNP" and techs_tuple[0] not in technologies_dict:
                 technologies_dict[techs_tuple[0]] = 1
+
+    technologies_dict["API"] = technologies_dict.get("API", 0) + technologies_dict.get(
+        "APIs", 0
+    )
+    del technologies_dict["APIs"]
+    technologies_dict["GIT"] = technologies_dict.get("GIT", 0) + technologies_dict.get(
+        "Git", 0
+    )
+    del technologies_dict["Git"]
 
     technologies_count_df = pd.DataFrame(
         list(technologies_dict.items()), columns=["Technology", "Popularity"]
@@ -31,22 +45,23 @@ def get_count_technologies() -> DataFrame:
 
 def diagram_of_popular_technologies() -> None:
     technologies = get_count_technologies().head(20)
-
-    plt.figure(figsize=(10, 7))
+    plt.figure(figsize=(9, 6))
     plt.bar(technologies["Technology"], technologies["Popularity"], width=0.5)
-    plt.xticks(technologies["Technology"], rotation="vertical")
+    plt.xticks(technologies["Technology"], rotation="vertical", fontsize=6)
+    plt.title("Top 20 popular technologies")
     plt.show()
 
 
-def correlation_diagram():
-    technologies = pd.read_csv("jobs.csv")
+def correlation_diagram() -> None:
+    technologies = pd.read_csv("../scraping/jobs.csv")
     technologies_corr = technologies[["experience", "views", "applications"]]
+    plt.figure(figsize=(5, 3))
     sns.heatmap(technologies_corr.corr(), annot=True, fmt=".2f", cmap="coolwarm")
     plt.title("Correlation Heatmap")
     plt.show()
 
 
-def generate_wordcloud():
+def generate_wordcloud() -> None:
     technologies_df = get_count_technologies().head(20)
     technologies = " ".join(technologies_df["Technology"].astype(str))
 
@@ -54,7 +69,7 @@ def generate_wordcloud():
         width=800, height=400, random_state=21, max_font_size=110
     ).generate(technologies)
 
-    plt.figure(figsize=(10, 7))
+    plt.figure(figsize=(5, 3))
     plt.imshow(wordcloud, interpolation="bilinear")
     plt.axis("off")
     plt.show()
